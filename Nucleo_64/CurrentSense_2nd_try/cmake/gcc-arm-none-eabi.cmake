11ac6aaf8470224c0502ae3ed33ae5c0bc51b8e1
DeatakCZ <tomasweb@centrum.cz>
2025-03-09 10:13:50 +0100

    printf float

--- Nucleo_64/CurrentSense_2nd_try/cmake/gcc-arm-none-eabi.cmake
+++ Nucleo_64/CurrentSense_2nd_try/cmake/gcc-arm-none-eabi.cmake
@@ -43,3 +43,5 @@ set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lc -lm -Wl,--en
 set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--print-memory-usage")
 
 set(CMAKE_CXX_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lstdc++ -lsupc++ -Wl,--end-group")
+
+set(FLAGS "-fdata-sections -ffunction-selection --specs=nano.specs -Wl,--gc-sections -u_printf_float")
